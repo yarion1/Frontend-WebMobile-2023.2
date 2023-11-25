81159640b04eee3d749c6b313518b53a9f0d7d3b
pablo henrique <yariongranham@gmail.com>
2023-11-25 17:15:09 -0300

    refact responsividade

--- src/components/Movies/Cast.vue
+++ src/components/Movies/Cast.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="container mx-auto  border-b border-gray-600 px-4 py-4">
-    <h2 class="text-4xl font-semibold mb-5">
+  <div class="container mx-auto border-b border-gray-600 px-4 py-4">
+    <h2 class="text-2xl md:text-4xl font-semibold mb-5">
       Cast
     </h2>
 
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-      <div class="mr-2" :key="index" v-for="(cast, index) in casts">
+    <div class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+      <div :key="index" v-for="(cast, index) in casts.slice(0,10)" class="mb-4">
         <img
           :src="castProfileImage(cast)"
           alt=""
-          class="hover:opacity-75 tansition easy-in-out duration-150"
+          class="w-full h-auto object-cover hover:opacity-75 transition ease-in-out duration-150"
         />
-        <span class="text-gray-300">{{ cast.name }}</span>
+        <span class="block mt-2 text-gray-300 text-sm truncate">{{ cast.name }}</span>
       </div>
     </div>
   </div>
